cfc6cb337933cf5d19414c2acee0a75972ca6a59
Vito Gentile <gentilevito1076@gmail.com>
2020-12-18 18:26:05 +0100

    4: Inserita TRACCIA + var Flag scordata ed inserita

--- 4/js/script.js
+++ 4/js/script.js
@@ -1,14 +1,21 @@
-var lista = [ 'Pippo', 'Paperino', 'Minni', 'Paperone', 'PLuto'];
+/*In un array sono contenuti i nomi degli invitati alla festa del
+grande Gatsby, chiedi all’utente il suo nome e comunicagli se
+può partecipare o no alla festa. */
+var lista = [ 'Pippo', 'Paperino', 'Minni', 'Paperone', 'Pluto'];
 var nome = prompt('Inserisci il tuo nome');
 nome = nome[0].toUpperCase() + nome.slice(1);
 console.log(nome);
 
-for (var i = 0; i < lista.length; i++) {
-  if (nome = lista[i]) {
-    console.log('Puoi entrare');
-  }
-  else if (nome != lista[i]) {
-    console.log('Non puoi entrare');
-  }
+var nomeTrovato = false;
 
+for (var i = 0; i < lista.length; i++) {
+  if (nome === lista[i]) {
+    nomeTrovato = true;
+  }
+}
+if (nomeTrovato === true) {
+  console.log('Puoi entrare');
+}
+else {
+  console.log('Non puoi entrare');
 }
