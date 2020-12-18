bdee83bddaefa7b0e7ca1fbf09c0c10b5b17d7aa
Vito Gentile <gentilevito1076@gmail.com>
2020-12-18 20:32:53 +0100

    1 OK: C'era un Bug trovato e corretto

--- 1/js/script.js
+++ 1/js/script.js
@@ -1,13 +1,29 @@
 /* L’utente inserisce due numeri in successione, con due
 prompt. Il software stampa il maggiore. */
-var firstNumber = prompt('Inserisci il primo numero');
+
+/*N.B.:  Se qui sotto non inserisco il parseINt e vado a confrontare un numero ( che di fatto resta una stringa) di 4 cifre con uno di 3 cifre ma con la prima cifra di quest'ultimo
+più grande della prima cifra del primo, l'output risultante è sballato.*/
+var firstNumber = parseInt(prompt('Inserisci il primo numero'));
 console.log(firstNumber);
-var secondNumber = prompt('Inserisci il secondo numero');
+var secondNumber = parseInt(prompt('Inserisci il secondo numero'));
 console.log(secondNumber);
 
-if ( firstNumber > secondNumber) {
+// if ( firstNumber > secondNumber) {
+//   console.log(firstNumber);
+// }
+// else if ( firstNumber < secondNumber) {
+//   console.log(secondNumber);
+// }
+
+
+
+// ALTERNATIVA Ciclo If + Completa:
+if ( firstNumber === secondNumber) {
+  alert('I numeri sono uguali');
+}
+else if (firstNumber > secondNumber) {
   console.log(firstNumber);
 }
-else if ( firstNumber < secondNumber) {
+else {
   console.log(secondNumber);
 }
